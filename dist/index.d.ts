3cfc6ec6f08b794154ee5ffb262579062f61cc55
HideakiYamamoto <poo3gumi@gmail.com>
2021-05-06 11:39:48 +0900

    #4 JSをビルドに含む

---
+++ dist/index.d.ts
@@ -0,0 +1,12 @@
+declare let a: number;
+declare let b: string;
+declare const c = "pineapples";
+declare let d: boolean[];
+declare let e: {
+    type: string;
+};
+declare let f: (number | boolean)[];
+declare let f2: (number | boolean)[];
+declare const g: number[];
+declare let g2: number[];
+declare let h: any;
